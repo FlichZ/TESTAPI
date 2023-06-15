6aee0faaa6fce3ce7facd1d2a19b38af6edf84ff
Flich <zubastik_bro@mail.ru>
2023-06-16 00:17:33 +0300

    ya_zamushalsya

---
+++ TestAPI2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenMeteo;
+
+namespace TestAPI2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task RunAsync()
+        {
+
+            OpenMeteoClient client = new OpenMeteoClient();
+
+            WeatherForecast weatherData = await client.QueryAsync("London");
+
+            Console.WriteLine(weatherData.CurrentWeather.Temperature + "°C");
+            Console.ReadKey();
+        }
+    }
+}
